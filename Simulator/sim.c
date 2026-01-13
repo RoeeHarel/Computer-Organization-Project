c5fd61d7ae46c17b99f3ba944cfdfd6693c0c3dc
Roee Harel <167644910+RoeeHarel@users.noreply.github.com>
2026-01-13 19:43:15 +0200

    Update sim.c
    added cases 15-17
    added a signExtension function

--- Simulator/sim.c
+++ Simulator/sim.c
@@ -26,14 +26,19 @@ unsigned int io_regs[23] = {0};
 unsigned int mainMem[MEM_SIZE] = {0}; 
 unsigned int hard_disk_arr[DISK_SIZE] = {0};
 unsigned int irq2_interrupt_cycles[MEM_SIZE] = { 0 }; // array of all the pc which has irq2in interrupt
+
 int branch_condition = FALSE; // A global variable used in the functions Execute() and AdvancePC()
 Instruction current_inst ; // initializing the variable for later use in Fetch()
 int interrupt_flag = 0;   // 1 if an interrupt occurred
+int mem_mask = 0xfffff; // ensures only the first 20 bits of a 32 bit integer are written into main memory
 
 // --- Function Initialization ---
 
 int inst_is_I_type(Instruction* inst);
 // returns TRUE if the instruction is I-type, and FALSE if it's R-type
+
+int signExtension(int var)
+// extends a 20 bit signed integer with 12 leading zeros, into a 32 bit signed integer
 
 Instruction Fetch ();
 // Fetches each instruction
@@ -41,7 +46,7 @@ Instruction Fetch ();
 void Execute (Instruction* inst);
 // Executes each instruction
 
-int AdvancePC(Instruction* inst);
+void AdvancePC(Instruction* inst);
 // advances pc according to the branch condition and type of the current instruction
 
 //Fills mainMem array, based on memin.txt file
@@ -55,10 +60,16 @@ void FillIrq2inArr(FILE * pirq2in);
 
 // --- Function Implementation ---
 
+// Setup Functions //
+
 int inst_is_I_type (Instruction* inst) {
 	return (inst->opcode >= 9 && inst->opcode <= 18) ? TRUE : FALSE;
 }
-// Setup Functions //
+
+int signExtension(int val) {
+    return (val << 12) >> 12; // shifts the 20-bit value to the MSB, and copies the MSB for the first 12 digits to extend the sign
+}
+
 void FillmainMem(FILE * pmemin) //TODO
 {
 
@@ -86,72 +97,82 @@ Instruction Fetch () {
 	regs[1] = inst->imm; // I think this needs to be here, it affects my part of the code (Roee)
 }
 
-void Execute(Instruction* inst) {
-	int branch_condition = FALSE;
+void Execute(Instruction* inst_ptr) {
+	// Reset the global variable branch_condition
+	branch_condition = FALSE;
+	int addr = regs[inst_ptr->rs] + regs[inst_ptr->rt];
 	
-	switch (inst->opcode) {
+	switch (inst_ptr->opcode) {
 		case 0: // add
-	        regs[inst->rd] = regs[inst->rs] + regs[inst->rt];
+	        regs[inst_ptr->rd] = regs[inst_ptr->rs] + regs[inst_ptr->rt];
 	        break;
 	    case 1: // sub
-	        regs[inst->rd] = regs[inst->rs] - regs[inst->rt];
+	        regs[inst_ptr->rd] = regs[inst_ptr->rs] - regs[inst_ptr->rt];
 	        break;
 	    case 2: // mul
-	        regs[inst->rd] = regs[inst->rs] * regs[inst->rt];
+	        regs[inst_ptr->rd] = regs[inst_ptr->rs] * regs[inst_ptr->rt];
 	        break;
 	    case 3: // and
-	        regs[inst->rd] = regs[inst->rs] & regs[inst->rt];
+	        regs[inst_ptr->rd] = regs[inst_ptr->rs] & regs[inst_ptr->rt];
 	        break;
 	    case 4: // or
-	        regs[inst->rd] = regs[inst->rs] | regs[inst->rt];
+	        regs[inst_ptr->rd] = regs[inst_ptr->rs] | regs[inst_ptr->rt];
 	        break;
 	    case 5: // xor
-	        regs[inst->rd] = regs[inst->rs] ^ regs[inst->rt];
+	        regs[inst_ptr->rd] = regs[inst_ptr->rs] ^ regs[inst_ptr->rt];
 	        break;
 	    case 6: // sll - Shifting rs by the value in rt
-	        regs[inst->rd] = regs[inst->rs] << regs[inst->rt];
+	        regs[inst_ptr->rd] = regs[inst_ptr->rs] << regs[inst_ptr->rt];
 	        break;
 	    case 7: // sra - Cast to signed int to ensure arithmetic shift (sign extension)
-	        regs[inst->rd] = (int)regs[inst->rs] >> regs[inst->rt];
+	        regs[inst_ptr->rd] = (int)regs[inst_ptr->rs] >> regs[inst_ptr->rt];
 	        break;
 	    case 8: // srl - Cast to unsigned int to ensure logical shift (zero fill)
-	        regs[inst->rd] = (unsigned int)regs[inst->rs] >> regs[inst->rt];
+	        regs[inst_ptr->rd] = (unsigned int)regs[inst_ptr->rs] >> regs[inst_ptr->rt];
 	        break;
 		case 9: // beq
-	        if (regs[inst->rs] == regs[inst->rt])
+	        if (regs[inst_ptr->rs] == regs[inst_ptr->rt])
 	            branch_condition = TRUE;
 	        break;
 	
 	    case 10: // bne
-	        if (regs[inst->rs] != regs[inst->rt])
+	        if (regs[inst_ptr->rs] != regs[inst_ptr->rt])
 	            branch_condition = TRUE;
 	        break;
 	
 	    case 11: // blt
-	        if ((int)regs[inst->rs] < (int)regs[inst->rt])
+	        if ((int)regs[inst_ptr->rs] < (int)regs[inst_ptr->rt])
 	            branch_condition = TRUE;
 	        break;
 	
 	    case 12: // bgt
-	        if ((int)regs[inst->rs] > (int)regs[inst->rt])
+	        if ((int)regs[inst_ptr->rs] > (int)regs[inst_ptr->rt])
 	            branch_condition = TRUE;
 	        break;
 	
 	    case 13: // ble
-	        if ((int)regs[inst->rs] <= (int)regs[inst->rt])
+	        if ((int)regs[inst_ptr->rs] <= (int)regs[inst_ptr->rt])
 	            branch_condition = TRUE;
 	        break;
 	
 	    case 14: // bge
-	        if ((int)regs[inst->rs] >= (int)regs[inst->rt])
+	        if ((int)regs[inst_ptr->rs] >= (int)regs[inst_ptr->rt])
 	            branch_condition = TRUE;
 	        break;
 	    case 15: // jal
+			regs[inst_ptr->rd] = pc + (inst_is_I_type(inst)) ? 2 : 1; // copied from AdvancePC()
+			pc = regs[inst_ptr->rs];
 	        break;
-	    case 16: // lw
-	        break;
-	    case 17: // sw
-	        break;
+		case 16: // lw (Load Word)
+            if (inst_ptr->rd > 1) // prevents writing into $zero and $imm
+			{
+                regs[inst_ptr->rd] = signExtension(mainMem[addr]);
+            }
+            break;
+
+        case 17: // sw (Store Word)
+            mainMem[addr] = regs[inst_ptr->rd] & mem_mask;
+            break;
 	    case 18: // reti
 	        break;
 	    case 19: // in
@@ -162,15 +183,16 @@ void Execute(Instruction* inst) {
 	        break;
 	    default: // need to implement
 	        break;
-
-	regs[0] = 0; // making sure $zero is unchanged
-	regs[1] = inst->imm; // making sure $imm is unchanged
 	}
+    
+    regs[0] = 0; // making sure $zero is unchanged
+    regs[1] = inst_ptr->imm; // making sure $imm is unchanged
 }
 
-AdvancePC(Instruction* inst) {
-	// need to also check for branch condition
-	pc += (inst_is_I_type(inst)) ? 2 : 1;
+void AdvancePC(Instruction* inst) {
+	if (branch_condition == TRUE)
+		pc = regs[inst->rd];
+	else pc += (inst_is_I_type(inst)) ? 2 : 1;
 }
 
 
